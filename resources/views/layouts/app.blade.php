5fa028139cff89b7cc5489ea843c793f2e6e897b
NobitaLu <lwx863503914@gmail.com>
2018-09-25 02:28:19 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - LaraBBS</title>
+    <title>@yield('title', 'LaraBBS') - LaraBBS </title>
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
