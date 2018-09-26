c1ff49bc8948abce5fc55677bedc16f1e008d31d
NobitaLu <lwx863503914@gmail.com>
2018-09-26 07:57:34 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,17 +3,13 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
-    {
-        //
-    }
-
     public function updating(Reply $reply)
     {
         //
@@ -21,7 +17,11 @@ class ReplyObserver
 
     public function created(Reply $reply)
     {
-        $reply->topic->increment('reply_count', 1);
+        $topic = $reply->topic;
+        $topic->increment('reply_count', 1);
+
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
