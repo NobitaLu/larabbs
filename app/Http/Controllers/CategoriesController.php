2a0c842cf09da8c0da29d6ced56e7934e9c23cf9
NobitaLu <lwx863503914@gmail.com>
2018-09-28 07:55:19 +0000

    Sidebar links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
@@ -17,7 +18,10 @@ class CategoriesController extends Controller
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
 
+        // 资源链接
+        $links = $link->getAllCached();
+
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
